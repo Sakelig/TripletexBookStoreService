ad4ac345e7cd5c1eaffbc27e45844b081e3bcb62
Steffan D. Klausen <steffan.pirtzl@awacademy.training>
2022-09-22 09:49:00 +0200

    🟢🟢🟢 Can now add new books :) 😂

--- src/main/java/no/academy/bookservice/Book/BookController.java
+++ src/main/java/no/academy/bookservice/Book/BookController.java
@@ -3,9 +3,7 @@ package no.academy.bookservice.Book;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
 
@@ -28,5 +26,12 @@ public class BookController {
         } else {
             throw new BookNotFoundException();
         }
+    }
+
+
+    @PostMapping("/book")
+    public Book newBook(@RequestBody Book book){
+        bookRepository.save(book);
+        return book;
     }
 }
